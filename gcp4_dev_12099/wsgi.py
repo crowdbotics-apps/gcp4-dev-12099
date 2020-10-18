e9609a46b688a1f7a9743b7c78a0adeec8f0ede8
Crowdbotics <team@crowdbotics.com>
2020-10-18 06:16:37 +0000

    Update models

--- gcp4_dev_12099/wsgi.py
+++ gcp4_dev_12099/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gcp4_dev_12099.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gcp4_dev_12099.settings")
 
 application = get_wsgi_application()
